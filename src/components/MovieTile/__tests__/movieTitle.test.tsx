61ea3f7397cf8837d4ef8681ed968db46ed1fbc1
Eduard Sardyka <eds832@gmail.com>
2023-09-21 17:03:28 +0200

    task3

--- src/components/MovieTile/__tests__/movieTitle.test.tsx
+++ src/components/MovieTile/__tests__/movieTitle.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import MovieTitle from '../MovieTile';
 import { MovieType } from './../../../store/movies/types';
